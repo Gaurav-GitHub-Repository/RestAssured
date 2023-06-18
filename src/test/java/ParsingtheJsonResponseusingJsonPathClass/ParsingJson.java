00b7305aea96cb5e873c340b3768eda3de9e3342
Pathange Gaurav <126910570+Gaurav-GitHub-Repository@users.noreply.github.com>
2023-06-18 12:52:37 +0530

    Update ParsingJson.java

--- src/test/java/ParsingtheJsonResponseusingJsonPathClass/ParsingJson.java
+++ src/test/java/ParsingtheJsonResponseusingJsonPathClass/ParsingJson.java
@@ -31,7 +31,7 @@ public class ParsingJson {
     		+ "")
     
     .when().post("maps/api/place/add/json").then().assertThat().statusCode(200)
-    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
     
     
   System.out.println(response);
