acf053cc4cf0950f6f71b8c04d2a9013dca9b372
Pathange Gaurav <126910570+Gaurav-GitHub-Repository@users.noreply.github.com>
2023-06-18 12:50:20 +0530

    Update API.java

--- src/test/java/IntegratingMultipleAPIwithJsonResponse/API.java
+++ src/test/java/IntegratingMultipleAPIwithJsonResponse/API.java
@@ -13,7 +13,7 @@ public class API {
 		
     RestAssured.baseURI= "https://rahulshettyacademy.com";
     
- String response =  RestAssured.given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+    String response =  RestAssured.given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
     .body("{\r\n"
     		+ "  \"location\": {\r\n"
     		+ "    \"lat\": -38.383494,\r\n"
@@ -35,15 +35,15 @@ public class API {
     .when().post("maps/api/place/add/json")
     .then().log().all().assertThat().statusCode(200)
     
-    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
     
     
     System.out.println(response);
     
     JsonPath js = new JsonPath(response);
-   String Place_ID = js.getString("place_id"); 
+    String Place_ID = js.getString("place_id"); 
    
-   System.out.println(Place_ID);
+    System.out.println(Place_ID);
 
     
     // Update place
