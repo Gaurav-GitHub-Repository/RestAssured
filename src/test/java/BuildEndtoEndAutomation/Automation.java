c11d823af1072f6e14e4c106f266a722f0842404
Pathange Gaurav <126910570+Gaurav-GitHub-Repository@users.noreply.github.com>
2023-06-18 12:17:30 +0530

    Update Automation.java

--- src/test/java/BuildEndtoEndAutomation/Automation.java
+++ src/test/java/BuildEndtoEndAutomation/Automation.java
@@ -38,7 +38,7 @@ public class Automation {
 		    .when().post("maps/api/place/add/json")
 		    .then().log().all().assertThat().statusCode(200)
 		    
-		    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+		    .body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		    
 		    
 		    System.out.println(response);
